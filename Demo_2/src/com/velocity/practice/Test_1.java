378bfc6173e346889e756dbea877f4c2b4a09b63
Akshay Potdar50 <potdarakshay50@gmail.com>
2022-11-15 15:20:11 +0530

    my first commit

---
+++ Demo_2/src/com/velocity/practice/Test_1.java
@@ -0,0 +1,17 @@
+package com.velocity.practice;
+
+import java.util.HashMap;
+public class Test_1  {
+
+	public static void main(String[] args) {
+		HashMap<Integer,String> hashmap = new HashMap<Integer,String>();
+		hashmap.put(11, "AKSHAY");
+		hashmap.put(12, "ASHISH");
+		hashmap.put(13, "SWAPNIL");
+		hashmap.put(14, "GANESH");
+		hashmap.put(15, "ABHI");
+		System.out.println(hashmap);
+		
+	}
+
+}
